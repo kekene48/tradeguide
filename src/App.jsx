b1a595fe7ed721f39a3be8244cce6543428510b7
kekene48 <kekene48@gmail.comgit config --global user.email kekene48@gmail.com>
2023-02-19 16:19:35 +0100

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,26 @@
-import './App.scss';
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import './index.scss';
+import Home from './Home/Home';
+import Main from './Main/Main';
+import Error from './Error'
 
-function App() {
+const App = () => {
   return (
-    <>
-     <div className="app">
-        <main className="app__main">
-          <div className="app__top">
-            <div className="app__logo">
-
-            </div>
-            <div className="app__title">Trade Guide</div>
-          </div>
-          <div className="app__motto">
-            <div className="app__motto-lg">
-              we strategize, you follow
-            </div>
-            <div className="app__motto-sm">
-              we provide a list of trades, you decide which to set up your trade as
-            </div>
-          </div>
-          <div className="app__btn">
-            Get Started
-          </div>
-        </main>
-
-        <section className="about">
-          <div className="about__card about__card-1">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab ad sit nesciunt laboriosam 
-            dolore autem quo. Fugit omnis tempore alias odit esse nihil explicabo molestiae incidunt, 
-            repellendus ea. Necessitatibus, facere.
-          </div>
-          <div className="about__card about__card-2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos architecto nisi laudantium 
-            earum pariatur excepturi, facere eum iusto voluptatum ullam blanditiis, provident dolorem 
-            perspiciatis dolor eius expedita? Asperiores, quasi culpa.
-          </div>
-          <div className="about__card about__card-3">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quae ducimus officiis! Illum saepe,
-            non est similique suscipit placeat. Possimus adipisci sapiente tempore harum iusto atque accusantium 
-            quae, quos quisquam.
-          </div>
-          <div className="about__card about__card-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quae ducimus officiis! Illum saepe,
-            non est similique suscipit placeat. Possimus adipisci sapiente tempore harum iusto atque accusantium 
-            quae, quos quisquam.
-          </div>
-        </section>
-    </div>
-    </>
-   
-  );
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/main'>
+          <Main />
+        </Route>
+        <Route path='*'>
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Home/Home.jsx
@@ -0,0 +1,56 @@
+import './home.scss';
+import { Link } from 'react-router-dom';
+
+function Home() {
+  return (
+    <>
+     <div className="app">
+        <main className="app__main">
+          <div className="app__top">
+            <div className="app__logo">
+
+            </div>
+            <div className="app__title">Trade Guide</div>
+          </div>
+          <div className="app__motto">
+            <div className="app__motto-lg">
+              we strategize, you follow
+            </div>
+            <div className="app__motto-sm">
+              we provide a list of trades, you decide which to set up your trade as
+            </div>
+          </div>
+          <Link to='/main' className='app__btn'>
+            Get Started
+          </Link>
+        </main>
+
+        <section className="about">
+          <div className="about__card about__card-1">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab ad sit nesciunt laboriosam 
+            dolore autem quo. Fugit omnis tempore alias odit esse nihil explicabo molestiae incidunt, 
+            repellendus ea. Necessitatibus, facere.
+          </div>
+          <div className="about__card about__card-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos architecto nisi laudantium 
+            earum pariatur excepturi, facere eum iusto voluptatum ullam blanditiis, provident dolorem 
+            perspiciatis dolor eius expedita? Asperiores, quasi culpa.
+          </div>
+          <div className="about__card about__card-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quae ducimus officiis! Illum saepe,
+            non est similique suscipit placeat. Possimus adipisci sapiente tempore harum iusto atque accusantium 
+            quae, quos quisquam.
+          </div>
+          <div className="about__card about__card-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quae ducimus officiis! Illum saepe,
+            non est similique suscipit placeat. Possimus adipisci sapiente tempore harum iusto atque accusantium 
+            quae, quos quisquam.
+          </div>
+        </section>
+    </div>
+    </>
+   
+  );
+}
+
+export default Home;
